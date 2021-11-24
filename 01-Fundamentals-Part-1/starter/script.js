50489468e8805c15f753424bb88fabfa74d997ee
PrinceHerenj <snehprince.herenj@gmail.com>
2021-11-24 19:52:30 +0530

    fundamentals1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,112 @@
+const country = 'India'
+const continent = 'Asia'
+let population = 138
+// let population =  49
+
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+
+// const isIsland = false
+// let language
+
+// console.log(isIsland)
+// console.log(population)
+// console.log(country)
+// console.log(language)
+
+const language = 'Hindi'
+// const language = 'English'
+
+// console.log(population / 2)
+// console.log(population + 1)
+// console.log(population > 6)
+// console.log(population < 33)
+// let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language
+// console.log(description)
+
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+
+// const string = `I'm ${firstName}, a ${2021 - birthYear} year old ${job}`;
+// console.log(string);
+
+// let description = `${country} is is ${continent}, and its ${population} million people speak ${language}`;
+// console.log(description)
+
+// if(population > 33) console.log(`${country}'s population is above average`)
+// else console.log(`${country}'s population is ${33-population} below aveage`)
+
+// const inputYear = '1991';
+// console.log(Number(inputYear) + 18);
+
+// // type coercion
+// console.log('I am ' + 23 + ' years old!');
+// console.log('23' - '10' - 3);
+// console.log('23' * '2');
+// console.log('23' / 2)
+
+// let n = '1' + 1
+// n = n - 1;
+// console.log(n)
+
+// console.log('9' - '5')
+// console.log('19' - '13' + '17')
+// console.log('19' - '13' + 17)
+// console.log('123' < 57)
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// console.log(Boolean(0))
+// console.log(Boolean(''))
+// console.log(Boolean(undefined))
+// console.log(Boolean(null))
+// console.log(Boolean(NaN))
+
+
+// let money = 0;
+
+// if(money) {
+//     console.log(`Don't spend it all`)
+// } else {
+//     console.log(`You should get a job!`)
+// }
+
+// let height
+// if(height) {
+//     console.log(`Height defined`)
+// } else {
+//     console.log(`Height not defined`)
+// }
+
+// let numNeighbours = prompt('How many neighbour countries does your country have?')
+// if(numNeighbours === 1) console.log(`Only 1 border!`);
+// else if(numNeighbours > 1) console.log(`More than 1 border`)
+// else console.log(`No borders`)
+
+// if(language == 'English' && population < 50 && !isIsland) {
+//     console.log(`You should live in ${country}`);
+// }
+
+// switch (language) {
+//     case 'Chinese': case 'Mandarin':
+//         console.log(`MOST number of native speakers`)
+//         break;
+//     case 'Spanish':
+//         console.log(`2nd place in number of native speakers`)
+//         break;
+//     case 'English':
+//         console.log(`3rd place`)
+//         break;
+//     case 'Hindi':
+//         console.log('Number 4')
+//         break;
+//     case 'Arabic':
+//         console.log(`5th most spoken language`)
+//         break;
+//     default:
+//         console.log('Great language too :D')
+//         break;
+// }
+
+console.log(`${country}'s population is ${population > 33 ?`above`: `below`} average`);
