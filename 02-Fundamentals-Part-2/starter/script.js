0471a3f74be4dba0a4d765913999334def8cd9f8
PrinceHerenj <snehprince.herenj@gmail.com>
2021-11-27 21:27:01 +0530

    yoo

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -15,12 +15,12 @@
 // logger()
 
 // function declarations
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples)
-    const orangePieces = cutFruitPieces(oranges)
-    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces`
-    return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples)
+//     const orangePieces = cutFruitPieces(oranges)
+//     const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces`
+//     return juice;
+// }
 
 // console.log(fruitProcessor(5, 0))
 // console.log(fruitProcessor(12, 4))
@@ -49,8 +49,19 @@ function fruitProcessor(apples, oranges) {
 // console.log(yearsUntilRetirement(2002, `Jonas`))
 // console.log(yearsUntilRetirement(1998, `Prashant`))
 
-function cutFruitPieces(fruit) {
-    return fruit * 4
+// function cutFruitPieces(fruit) {
+//     return fruit * 4
+// }
+
+// console.log(fruitProcessor(2, 3))
+
+const calcAge = birthYear => 2021 - birthYear
+
+const yearsUntilRetirement = function(birthYear, firstName) {
+        const age = calcAge(birthYear)
+        const retirement = 65 - age;
+        return retirement > 0 ? `${firstName} retires in ${retirement} years` : `Already retired`
 }
 
-console.log(fruitProcessor(2, 3))
+console.log(yearsUntilRetirement(2002, `Prince`))
+console.log(yearsUntilRetirement(1970, `Mike`))
