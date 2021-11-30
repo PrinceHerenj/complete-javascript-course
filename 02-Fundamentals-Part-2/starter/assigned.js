08b24d23fa7b3e54484356cdf10a84c0c0acd798
PrinceHerenj <snehprince.herenj@gmail.com>
2021-11-30 09:02:40 +0530

    assignment 2 half

---
+++ 02-Fundamentals-Part-2/starter/assigned.js
@@ -0,0 +1,50 @@
+// function describeCountry(country, population, capitalCity) {
+//     return `${country} has ${population} million people and its capital city is ${capitalCity}`
+// }
+
+// const india = describeCountry(`India`, 138, `Delhi`)
+// console.log(india)
+// const finland = describeCountry(`Finland`, 6, `Helsinki`)
+// console.log(finland)
+
+// function percentageOfWorld1(population) {
+//     return Math.round((population / 7900) * 100 * 100) / 100
+// }
+
+// const per1 = percentageOfWorld1(1393)
+// console.log(per1)
+// const per2 = percentageOfWorld1(1441)
+// console.log(per2)
+// const per3 = percentageOfWorld1(6)
+// console.log(per3)
+
+// const percentageOfWorld2 = function(population) {
+//     return Math.round((population / 7900) * 100 * 100) / 100
+// }
+
+// const per4 = percentageOfWorld2(1393)
+// console.log(per4)
+// const per5 = percentageOfWorld2(1441)
+// console.log(per5)
+// const per6 = percentageOfWorld2(6)
+// console.log(per6)
+
+// const percentageOfWorld3 = population => Math.round((population / 7900) * 100 * 100) / 100
+
+// // console.log(percentageOfWorld3(1393))
+// // console.log(percentageOfWorld3(1441))
+// // console.log(percentageOfWorld3(6))
+
+// const describePopulation = (country, population) => `${country} has ${population} million people, which is about ${percentageOfWorld3(population)}% of the world`
+// console.log(describePopulation(`India`, 1393))
+
+// const populations = [1393, 1441, 6, 138]
+// console.log(`${populations.length == 4}`)
+// const percentages = []
+// percentages[0] = percentageOfWorld3(populations[0])
+// percentages[1] = percentageOfWorld3(populations[1])
+// percentages[2] = percentageOfWorld3(populations[2])
+// percentages[3] = percentageOfWorld3(populations[3])
+// console.log(percentages)
+
+
