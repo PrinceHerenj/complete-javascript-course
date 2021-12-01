d54e7ea5e35c975e4526505f34b2679ff892a77f
PrinceHerenj <snehprince.herenj@gmail.com>
2021-12-01 20:49:13 +0530

    developer skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+let x = 2;
+if (x == 2) console.log(23);
 
+console.log(`Hello`);
+
+const calcAge = birthyear => 2037 - birthyear;
+
+console.log(calcAge(2021));
+console.log(x);
