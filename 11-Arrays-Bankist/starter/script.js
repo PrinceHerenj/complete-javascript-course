5f1a9dab5f744026af06cbf329119908ead66de8
PrinceHerenj <snehprince.herenj@gmail.com>
2022-05-31 12:19:56 +0530

    aaaa

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,90 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+/////////////////////////////////////////////////
+
+// // SLICE
+// let arr = [`a`, `b`, `c`, `d`, `e`];
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-3));
+// console.log(arr.slice(1, -2));
+
+// console.log(arr.slice());
+// console.log([...arr]); // creating shallow copies
+
+// // SPLICE
+// // console.log(arr.splice(2));
+// arr.splice(-1); // remove last element
+// // SPLICE mutates original array, returns slice and then removes sliced from array
+// arr.splice(1, 2);
+// console.log(arr);
+
+// // REVERESE
+// arr = [`a`, `b`, `c`, `d`, `e`];
+// const arr2 = [`j`, `i`, `h`, `g`, `f`];
+// console.log(arr2.reverse());
+// // // REVERSE mutates the array
+// // console.log(arr2);
+
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// // JOIN
+// console.log(letters.join(` - `));
+
+/////////////////////////////////////////////////
+
+// // AT
+// const arr = [23, 11, 64];
+// console.log(arr.at(0));
+
+// console.log(arr);
+// console.log(arr.at(-1));
+
+// console.log(`prince`.at(-1)); // also works with strings
+
+// // FOR EACH
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// for (const movement of movements) { // OR
+// for(const [index, movement] of movements.entries()) {
+
+//   if (movement > 0) console.log(`Movement ${i+1}: You deposited ${movement}`);
+//   else console.log(`Movement ${i+1}: You withdrew ${Math.abs(movement)}`);
+// }
+
+// movements.forEach(movement => {
+//   if (movement > 0) console.log(`You deposited ${movement}`);
+//   else console.log(`You withdrew ${Math.abs(movement)}`);
+// });
+// 0: function(200)
+// 1: function(450)... until end
+
+// movements.forEach((mov, i, arr) => {
+//   if (mov > 0) console.log(`Movement ${i + 1}:You deposited ${mov}`);
+//   else console.log(`Movement ${i + 1}:You withdrew ${Math.abs(mov)}`);
+// });
+
+// coninue and break statements dont work in a forEach loop
+
+/////////////////////////////////////////////////
+
+// MAP
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function (val, key) {
+  console.log(`${key}: ${val}`);
+});
 
-/////////////////////////////////////////////////
+const currenciesUnique = new Set([`USD`, `GBP`, `USD`, `EUR`, `EUR`]);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (val, _val) {
+  console.log(`${_val}: ${val}`);
+});
+
+// _val is throwaway variable
