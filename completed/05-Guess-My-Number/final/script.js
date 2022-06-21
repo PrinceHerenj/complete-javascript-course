3cee6d5bdd6a734878d8ffab6d673e0b1e0c5dc9
PrinceHerenj <snehprince.herenj@gmail.com>
2022-06-21 07:48:55 +0530

    heh

--- completed/05-Guess-My-Number/final/script.js
+++ completed/05-Guess-My-Number/final/script.js
@@ -99,7 +99,7 @@ document.querySelector('.again').addEventListener('click', function () {
 // Coding Challenge #1
 
 /* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
+Implement a game reset functionality, so that the player can make a new guess! Here is how:
 
 1. Select the element with the 'again' class and attach a click event handler
 2. In the handler function, restore initial values of the score and secretNumber variables
