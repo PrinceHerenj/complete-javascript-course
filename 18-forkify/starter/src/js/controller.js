1350b73c7a7fa135b48d684e9f330cbb8d0bf3b2
PrinceHerenj <snehprince.herenj@gmail.com>
2023-10-20 12:33:22 +0530

    293

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,3 +1,8 @@
+import * as model from './model.js'
+import recipeView from './views/recipeView.js'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -8,6 +13,35 @@ const timeout = function (s) {
   });
 };
 
+
+
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+// 11b35751-8769-4398-83c3-d40190364d6e
+// https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=<insert your key>
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return
+
+    recipeView.renderSpinner()
+
+    // 1) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering Recipe
+    recipeView.render(model.state.recipe)
+
+  }
+  catch (err) {
+    alert(err)
+  }
+};
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes))
+
+// ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes))
+
+// window.addEventListener("hashchange", controlRecipes)
