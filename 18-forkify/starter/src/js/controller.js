76642f4e56c52a38ec6499d4371dd3b92f364d72
PrinceHerenj <snehprince.herenj@gmail.com>
2023-11-02 15:19:32 +0530

    297 complete

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -2,14 +2,12 @@ import * as model from './model.js'
 import recipeView from './views/recipeView.js'
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-// 11b35751-8769-4398-83c3-d40190364d6e
-// https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=<insert your key>
+// if (module.hot) {
+//   module.hot.accept()
+// }
 
 const controlRecipes = async function () {
   try {
@@ -26,11 +24,30 @@ const controlRecipes = async function () {
 
   }
   catch (err) {
-    console.log(err)
+    recipeView.renderError()
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner()
+    // 1) Get search query
+    const query = searchView.getQuery()
+    if (!query) return
+
+    // 2) Load search results
+    await model.loadSearchResults(query)
+
+    // 3) Render results
+    resultsView.render(model.state.search.results)
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes)
+  searchView.addHandlerRender(controlSearchResults)
 }
+
 init();
